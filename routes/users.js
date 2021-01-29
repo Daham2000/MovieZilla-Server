38fb9e3c39edeb08fd169563216231211a5cbcfd
Daham2000 <dhmakla2000@gmail.com>
2021-01-29 12:16:29 +0530

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 import express from "express";
 
 import { signup, login } from "../controllers/register.js";
+import { getPosts,likePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.post('/createAccount',signup);
 router.post('/login',login);
+router.get('/getPosts',getPosts);
+router.patch('/:id/likePost',likePost);
 
 export default router;
