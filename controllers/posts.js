b94c02fe557a0b83342effc034571e15c7b480a6
Daham2000 <dhmakla2000@gmail.com>
2021-01-31 07:46:14 +0530

    first commit

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,91 +1,128 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import PostMessage from "../models/postMassage.js";
-import liked from '../models/likedModel.js';
- 
- export const getPosts =async (req,res) => {
-     try{
-        const postMessage = await PostMessage.find();
-        res.status(200).json(postMessage);
-     }catch(error) {
-        res.status(404).json({message: error.message});
-     }
- }
+import liked from "../models/likedModel.js";
 
- export const createPost =async(req,res) => {
-    const post = req.body;
-    const newPost = new PostMessage(post);
-    try {
-        await newPost.save();
-        res.status(201).json(newPost);
-    } catch (error) {
-        res.status(409).json({message: error.message});
-    }
-}
+export const getPosts = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
-export const updatePost = async(req,res) => {
-    const {id: _id} =req.params;
-    const post = req.body;
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No datails for that Id.");
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new:true});
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No datails for that Id.");
 
-    res.json(updatePost);
-}
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true }
+  );
 
-export const deletePost = async(req,res) => {
-    const {id} = req.params;
+  res.json(updatePost);
+};
 
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
 
-    await PostMessage.findByIdAndRemove(id);
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
 
-    res.json({massage: "Post deleted successfully"})
+  await PostMessage.findByIdAndRemove(id);
 
-}
+  res.json({ massage: "Post deleted successfully" });
+};
 
-export const ifAlreadyLiked = async(req ,res ) => {
-    const {id} = req.params;
-    const {email} = req.params;
+export const ifAlreadyLiked = async (req, res) => {
+  const { id } = req.params;
+  const { email } = req.params;
 
-    const newLike = new liked();
-    newLike.postId = id;
-    newLike.email = email;
-    try {
-        const r= await newLike.findOne(newLike);
-        console.log(r);
-        res.json({
-            'isExits': false
-        })
-    } catch (error) {
-        res.json({
-            'isExits': true
-        })
-    }
+  const newLike = new liked();
+  newLike.postId = id;
+  newLike.email = email;
+  try {
+    const r = await newLike.findOne(newLike);
+    console.log(r);
+    res.json({
+      isExits: false,
+    });
+  } catch (error) {
+    res.json({
+      isExits: true,
+    });
+  }
+};
 
-}
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  const { email } = req.params;
 
-export const likePost = async(req,res) => {
-    const {id} = req.params;
-    const {email} = req.params;
-
-    try {
-        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
-        const post = await PostMessage.findById(id);
-        const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount+1}, {new:true} );
-        res.json({
-            updatedPost,
-        });
-    } catch (error) {
-        res.json(error);
-    }
-}
-
-export const unlikePost = async(req,res) => {
-    const {id} = req.params;
-    await liked.findByIdAndRemove(id);
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send("No post with that id");
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount-1}, {new:true} );
-    res.json(updatedPost);
-}
+    const updatedPost = await PostMessage.findByIdAndUpdate(
+      id,
+      { likeCount: post.likeCount + 1 },
+      { new: true }
+    );
+    res.json({
+      updatedPost,
+    });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+export const unlikePost = async (req, res) => {
+  const { id } = req.params;
+  await liked.findByIdAndRemove(id);
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount - 1 },
+    { new: true }
+  );
+  res.json(updatedPost);
+};
+
+export const findMovies = async (req, res) => {
+
+  try {
+    const posts =await PostMessage.find( { tags: "Movie" } );
+    res.status(200).json(posts);
+  } catch (error) {
+    res.json(error);
+  }
+
+};
+
+export const findTvSeries = async (req, res) => {
+
+  try {
+    const posts = await PostMessage.find( { tags: "Tvseries" } );
+    res.status(200).json(posts);
+  } catch (error) {
+    res.json(error);
+  }
+
+};
+
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { signup, login } from "../controllers/register.js";
-import { getPosts,likePost,unlikePost,ifAlreadyLiked } from "../controllers/posts.js";
+import { getPosts,likePost,unlikePost,ifAlreadyLiked,findMovies, findTvSeries } from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -11,5 +11,7 @@ router.get('/getPosts',getPosts);
 router.patch('/:id/:email/likePost',likePost);
 router.patch('/:id/unlikePost',unlikePost);
 router.get('/:id/:email/ifAlreadyLiked', ifAlreadyLiked);
+router.get('/getMovies', findMovies);
+router.get('/getTvSeries', findTvSeries);
 
 export default router;
